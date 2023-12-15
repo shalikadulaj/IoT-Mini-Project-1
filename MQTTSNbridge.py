fb5380cadfa32a319c7ffa2e2a6729e3ac0e8448
Kavi91 <153508129+Kavi91@users.noreply.github.com>
2023-12-15 05:09:03 +0200

    Update MQTTSNbridge.py

--- MQTTSNbridge.py
+++ MQTTSNbridge.py
@@ -1,48 +1,47 @@
 from AWSIoTPythonSDK.MQTTLib import AWSIoTMQTTClient
 import MQTTSNclient
 import json
-#import boto3
 
-#connection to DynamoDB and access to the table EnvironmentalStationDB
-#dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
-#dynamoTable = dynamodb.Table('EnvironmentalStationDB')
 jsonP = ''
 
-# clients for MQTT and MQTTS
+# Clients for MQTT and MQTTS
 MQTTClient = AWSIoTMQTTClient("MQTTSNbridge")
 MQTTSNClient = MQTTSNclient.Client("bridge", port=1885)
 
 class Callback:
-  # function that replies a message from the MQTTSN broker to the MQTT one
-  # and inserts into the database the message just arrived
-  def messageArrived(self, topicName, payload, qos, retained, msgid):
-      message = payload.decode("utf-8")
-      jsonP = json.loads(message) 
-      print(topicName, message)
-      MQTTClient.publish(topicName, message, qos)
-      #dynamoTable.put_item(Item=jsonP)
-      return True
+    # Function that replies a message from the MQTTSN broker to the MQTT one
+    # and inserts into the database the message just arrived
+    def messageArrived(self, topicName, payload, qos, retained, msgid):
+        message = payload.decode("utf-8")
+        jsonP = json.loads(message)
+        print(topicName, message)
+        MQTTClient.publish(topicName, message, qos)
+        # DynamoDB integration (commented out for simplicity)
+        # dynamoTable.put_item(Item=jsonP)
+        return True
 
 
-# path that indicates the certificates position (ssh root@node-a8-?)
+# Path that indicates the certificates position (ssh root@node-a8-?)
 path = "/home/root/IoT-Mini-Project-1/certs/"
 
-# configure the access with the AWS MQTT broker
+# Configure the access with the AWS MQTT broker
 MQTTClient.configureEndpoint("a5hi9k1blxeai-ats.iot.eu-west-1.amazonaws.com", 8883)
-MQTTClient.configureCredentials(path+"AmazonRootCA1.pem",
-                                path+"41caa97615d76184172427b6764bded91448dede2bfe1f8a7411c59de3300978-private.pem.key",
-                                path+"41caa97615d76184172427b6764bded91448dede2bfe1f8a7411c59de3300978-certificate.pem.crt")
+MQTTClient.configureCredentials(
+    path + "AmazonRootCA1.pem",
+    path + "41caa97615d76184172427b6764bded91448dede2bfe1f8a7411c59de3300978-private.pem.key",
+    path + "41caa97615d76184172427b6764bded91448dede2bfe1f8a7411c59de3300978-certificate.pem.crt"
+)
 
-# configure the MQTT broker
+# Configure the MQTT broker
 MQTTClient.configureOfflinePublishQueueing(-1)  # Infinite offline Publish queueing
 MQTTClient.configureDrainingFrequency(2)  # Draining: 2 Hz
 MQTTClient.configureConnectDisconnectTimeout(10)  # 10 sec
 MQTTClient.configureMQTTOperationTimeout(5)  # 5 sec
 
-# register the callback
+# Register the callback
 MQTTSNClient.registerCallback(Callback())
 
-# make connections to the clients
+# Make connections to the clients
 MQTTClient.connect()
 MQTTSNClient.connect()
 
@@ -56,16 +55,16 @@ while True:
     else:
         station_ids += current_id + " "
 
-# subscribe to the topics choosen by the user
+# Subscribe to the topics chosen by the user
 for id in station_ids:
     MQTTSNClient.subscribe("sensor/station" + id)
 print("Subscribed to stations with ID: " + station_ids)
 
-# cycle that wait for a command to close the program
+# Cycle that waits for a command to close the program
 while True:
-    if input("Enter 'quit' to exit from the program.\n")=="quit":
+    if input("Enter 'quit' to exit from the program.\n") == "quit":
         break
-      
-# disconnect from the clients
+
+# Disconnect from the clients
 MQTTSNClient.disconnect()
 MQTTClient.disconnect()
